b86cd1526111a76fc6f345eb545a94bdc2599c7b
parth-darandale <parthdarandale5@gmail.com>
2025-03-22 11:29:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('map.html', api_key="AIzaSyAc5ri3Pk03W27nrZZePettuXcJDngo9Gs")
+    return render_template('map.html', api_key="")
 
 if __name__ == '__main__':
     app.run(debug=True)
